cbcab8f08cade260b1e358f600ac69a208d15136
hotdoghotdoghot <37475489+hotdoghotdoghot@users.noreply.github.com>
2018-11-19 19:10:25 -0500

    Added completed BallotTest and empty testing classes

---
+++ Gangsta-MidtermElectionApp/src/com/csci360/electionapp/controller/ConfigureBallotControllerTest.java
@@ -0,0 +1,20 @@
+package com.csci360.electionapp.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConfigureBallotControllerTest {
+    @Test
+    public void showBallotDetails() throws Exception {
+    }
+
+    @Test
+    public void updateDetails() throws Exception {
+    }
+
+    @Test
+    public void deleteBallot() throws Exception {
+    }
+
+}
---
+++ Gangsta-MidtermElectionApp/src/com/csci360/electionapp/controller/EditBallotControllerTest.java
@@ -0,0 +1,13 @@
+package com.csci360.electionapp.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EditBallotControllerTest {
+    @Test
+    public void setBallot() throws Exception
+    {
+    }
+
+}
